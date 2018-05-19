84badd8a1e1a5f5dc43d86c3b0a4cfe1cdf12a58
FrancesNK <shiyufeng1990@foxmail.com>
2018-05-19 06:52:37 +0000

    add pre-launch for receiver

--- bluetooth_prelaunch.py
+++ bluetooth_prelaunch.py
@@ -1,10 +1,14 @@
 import bluetooth
+import os
   
 print("performing inquiry...")
   
 nearby_devices = bluetooth.discover_devices(lookup_names = True)
   
-print("found %d devices" % len(nearby_devices))
+#print("found %d devices" % len(nearby_devices))
   
 for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
+    #print("  %s - %s" % (addr, name))
+    if name == 'techfair':
+        os.system('/opt/Citrix/ICAClient/util/pnabrowse -L "Windows 2012 R2 Desktop" -U administrator -P citrix -D tf.local http://10.158.141.214/citrix/store/pnagent/config.xml')
+        exit
